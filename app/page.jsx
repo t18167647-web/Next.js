5728d2a602d6e6c1575d45c348ed95a63ed9360f
t18167647-web <t18167647@gmail.com>
2026-03-28 22:30:59 +0900

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,47 @@
 "use client";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>投手管理アプリ</h1>
+    <div
+      style={{
+        padding: 20,
+        maxWidth: 500,
+        margin: "0 auto",
+        textAlign: "center",
+      }}
+    >
+      <h1 style={{ marginBottom: 30 }}>投手管理</h1>
 
-      <a href="/input">
-        <button style={{ marginTop: "10px" }}>入力する</button>
-      </a>
+      <Link href="/input">
+        <button
+          style={{
+            width: "100%",
+            padding: 20,
+            fontSize: 20,
+            marginBottom: 15,
+            borderRadius: 15,
+            background: "#0070f3",
+            color: "white",
+          }}
+        >
+          入力する
+        </button>
+      </Link>
 
-      <br />
-
-      <a href="/table">
-        <button style={{ marginTop: "10px" }}>一覧を見る</button>
-      </a>
+      <Link href="/table">
+        <button
+          style={{
+            width: "100%",
+            padding: 20,
+            fontSize: 20,
+            borderRadius: 15,
+            background: "#eee",
+          }}
+        >
+          結果を見る
+        </button>
+      </Link>
     </div>
   );
 }
