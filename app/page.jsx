35ff8f506fa545813ed1b681aca9b0d59e9bd34e
t18167647-web <t18167647@gmail.com>
2026-03-30 21:27:33 +0900

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,36 @@
-import Link from "next/link";
+"use client";
 
-export default function Home() {
+import { useEffect, useState } from "react";
+import { db } from "../../lib/firebase";
+import { collection, getDocs } from "firebase/firestore";
+
+export default function TablePage() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, "pitch_data"));
+      const list = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      setData(list);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div style={{ padding: 20 }}>
-      <h1>トップ</h1>
+      <h1>一覧</h1>
 
-      <a href="/input">入力ページへ</a>
-      <br />
-      <a href="/table">一覧ページへ</a>
+      {data.map((item) => (
+        <div key={item.id}>
+          {item.name} : {item.score}
+        </div>
+      ))}
     </div>
   );
 }
-
 
 
